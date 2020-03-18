a1b208d6b64e3d717e2756203f6c905840f36bd0
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2020-03-18 15:53:25 +0100

    minor changes in script

--- script.sh
+++ script.sh
@@ -1,45 +1,54 @@
 #!/usr/bin/env bash
 
+while [ -z $public_ip ]; do
+        read -p "Please enter yout machine public ip: " public_ip
+done
+
+# Create Openvidu user
+useradd openvidu
+
 # Install KMS
-sudo echo "deb [arch=amd64] http://ubuntu.openvidu.io/6.13.0 xenial kms6" | sudo tee /etc/apt/sources.list.d/kurento.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5AFA7A83
-sudo apt-get update
-sudo apt-get -y install kurento-media-server
+echo "deb [arch=amd64] http://ubuntu.openvidu.io/6.13.0 xenial kms6" | sudo tee /etc/apt/sources.list.d/kurento.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5AFA7A83
+apt-get update
+apt-get -y install kurento-media-server
+
+# Modify kurento-media-server
+sudo sed -i "s/DAEMON_USER=\"kurento\"/DAEMON_USER=\"openvidu\"/g" /etc/default/kurento-media-server
 
 # Install COTURN
-sudo apt-get -y install coturn
+apt-get -y install coturn
 
 # Install Redis
-sudo apt-get -y install redis-server
+apt-get -y install redis-server
 
 # Modify WebRtcEndpoint.conf.ini file
-sed 's/;externalAddress=.*/externalAddress=192.168.1.3/' /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+sed -r 's/;externalAddress=.*/externalAddress=192.168.1.3/' /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
 
 # Modify turnserver.conf file
-sed 's/#external-ip=.*/external-ip=192.168.1.3/' /etc/turnserver.conf
-sed 's/#listening-port=.*/listening-port=3478/' /etc/turnserver.conf
-sed 's/#fingerprint/fingerprint/' /etc/turnserver.conf
-sed 's/#lt-cred-mech/lt-cred-mech/' /etc/turnserver.conf
-sed 's/#max-port=.*/max-port=65535/' /etc/turnserver.conf
-sed 's/#min-port=.*/min-port=40000/' /etc/turnserver.conf
-sed 's/#pidfile=.*/pidfile="/var/run/turnserver.pid"/' /etc/turnserver.conf
-sed 's/#realm=.*/realm=openvidu/' /etc/turnserver.conf
-sed 's/#simple-log/simple-log/' /etc/turnserver.conf
-sed 's/#redis-userdb=.*/redis-userdb="ip=127.0.0.1 dbname=0 password=turn connect_timeout=30"/' /etc/turnserver.conf
-sed 's/#verbose/verbose/' /etc/turnserver.conf
+sed -r 's/#external-ip=.*/external-ip=192.168.1.3/' /etc/turnserver.conf
+sed -r 's/#listening-port=.*/listening-port=3478/' /etc/turnserver.conf
+sed -r 's/#fingerprint/fingerprint/' /etc/turnserver.conf
+sed -r 's/#lt-cred-mech/lt-cred-mech/' /etc/turnserver.conf
+sed -r 's/#max-port=.*/max-port=65535/' /etc/turnserver.conf
+sed -r 's/#min-port=.*/min-port=40000/' /etc/turnserver.conf
+sed -r 's/#pidfile=.*/pidfile="/var/run/turnserver.pid"/' /etc/turnserver.conf
+sed -r 's/#realm=.*/realm=openvidu/' /etc/turnserver.conf
+sed -r 's/#simple-log/simple-log/' /etc/turnserver.conf
+sed -r 's/#redis-userdb=.*/redis-userdb="ip=127.0.0.1 dbname=0 password=turn connect_timeout=30"/' /etc/turnserver.conf
+sed -r 's/#verbose/verbose/' /etc/turnserver.conf
 
 # Modify coturn file
 sed 's/#TURNSERVER_ENABLED=.*/TURNSERVER_ENABLED=1/' /etc/default/coturn
 
 # Restart services
-sudo service redis-server restart
-sudo service coturn restart
-sudo service kurento-media-server restar
+service redis-server restart
+service coturn restart
+service kurento-media-server restar
 
 # Install Java 8
-sudo apt-get install -y openjdk-8-jre
+apt-get install -y openjdk-8-jre
 
 # Install Openvidu
 last_openvidu_release=$(wget -q https://github.com/OpenVidu/openvidu/releases/latest -O - | grep -E \/tag\/ | awk -F "[><]" '{print $3}' | sed ':a;N;$!ba;s/\n//g' | sed 's/v//')
 wget "https://github.com/OpenVidu/openvidu/releases/download/v${last_openvidu_release}/openvidu-server-${last_openvidu_release}.jar"
-

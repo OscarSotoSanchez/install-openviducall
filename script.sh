bf4c492aa889132400c3e7bec36105bc76533f07
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2020-03-19 12:18:37 +0100

    Minnor changes

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-while [ -z $public_ip ]; do
+while [ -z "$public_ip" ]; do
         read -p "Please enter yout machine public ip: " public_ip
 done
 
@@ -24,6 +24,8 @@ apt-get -y install redis-server
 
 # Modify WebRtcEndpoint.conf.ini file
 sed -i "0,/;externalAddress=.*/s//externalAddress=${public_ip}/" /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+sed -i "0,/;stunServerAddress=.*/s//stunServerAddress=${public_ip}/" /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+sed -i "0,/;stunServerPort=.*/s//stunServerPort=3478/" /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
 
 # Modify turnserver.conf file
 sed -i "0,/#external-ip=.*/s//external-ip=${public_ip}/" /etc/turnserver.conf
@@ -41,16 +43,73 @@ sed -i '0,/#verbose/s//verbose/' /etc/turnserver.conf
 # Modify coturn file
 sed -i '0,/#TURNSERVER_ENABLED=.*/s//TURNSERVER_ENABLED=1/' /etc/default/coturn
 
-# Restart services
-service redis-server restart
-service coturn restart
-service kurento-media-server restar
-
 # Install Java 8
 apt-get install -y openjdk-8-jre
 
 # Install Openvidu
 mkdir /opt/openvidu
 last_openvidu_release=$(wget -q https://github.com/OpenVidu/openvidu/releases/latest -O - | grep -E \/tag\/ | awk -F "[><]" '{print $3}' | sed ':a;N;$!ba;s/\n//g' | sed 's/v//')
-wget -O /opt/openvidu/openvidu.jar "https://github.com/OpenVidu/openvidu/releases/download/v${last_openvidu_release}/openvidu-server-${last_openvidu_release}.jar"
+wget -O /opt/openvidu/openvidu-server.jar "https://github.com/OpenVidu/openvidu/releases/download/v${last_openvidu_release}/openvidu-server-${last_openvidu_release}.jar"
 chown -R openvidu:openvidu /opt/openvidu
+
+cat > /opt/openvidu/openvidu-server.sh<<EOF
+#!/bin/bash
+
+# This script will launch OpenVidu Server on your machine
+
+OPENVIDU_SECRET="prueba"
+
+OPENVIDU_OPTIONS="-Dopenvidu.secret=$OPENVIDU_SECRET "
+OPENVIDU_OPTIONS+="-Dserver.ssl.enabled=false "
+OPENVIDU_OPTIONS+="-Dopenvidu.publicurl=https://$public_ip:4443 "
+OPENVIDU_OPTIONS+="-Dserver.port=5443 "
+
+exec java -jar ${OPENVIDU_OPTIONS} /opt/openvidu/openvidu-server.jar
+EOF 
+
+# Install nginx
+apt-get install -y nginx
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+# ln -s /etc/nginx/sites-available/openvidu-call.conf /etc/nginx/sites-enabled/openvidu-call.conf
+
+# Install Openvidu Call
+mkdir /var/www/openvidu-call
+last_openvidu_call_release=$(wget -q https://github.com/OpenVidu/openvidu-call/releases/tag/v2.12.0 -O - | grep -E \/tag\/ | awk -F "[><]" '{print $3}' | sed ':a;N;$!ba;s/\n//g' | sed 's/v//')
+-L -o /var/www/openvidu-call/openvidu-call.tar.gz "https://github.com/OpenVidu/openvidu-call/releases/download/v${last_openvidu_call_release}/openvidu-call-${last_openvidu_call_release}.tar.gz"
+tar zxf /var/www/openvidu-call/openvidu-call.tar.gz -C /var/www/openvidu-call
+rm /var/www/openvidu-call/openvidu-call.tar.gz
+
+cat > /var/www/openvidu-call/ov-settings.json<<EOF
+{
+        "openviduSettings": {
+                "chat": true,
+                "autopublish": false,
+                "toolbarButtons": {
+                        "audio": true,
+                        "video": true,
+                        "screenShare": true,
+                        "fullscreen": true,
+                        "exit": true
+                }
+        },
+        "openviduCredentials": {
+                "openvidu_url": "http://192.168.1.40:4443",
+                "openvidu_secret": "prueba"
+        }
+}
+EOF 
+
+chown -R www-data.www-data /var/www/openvidu-call
+
+
+# Restart services
+service redis-server restart
+service coturn restart
+service kurento-media-server restar
+service nginx restart
+
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+update-rc.d kurento-media-server defaults
